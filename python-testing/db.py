0cb8817c9ac93d1011af6b11ddb144cf75491fc0
Hannah Rosete <hannahmae.rosete@gmail.com>
2026-08-06 01:54:58 +1000

    add: db.py
    Signed-off-by: Hannah Rosete <hannahmae.rosete@gmail.com>

---
+++ python-testing/db.py
@@ -0,0 +1,20 @@
+class Database:
+    """Simulate a basic user database."""
+
+    def __init__(self):
+        self.data = {}  # Simulating an in-memory database with a dictionary
+
+    def add_user(self, user_id, name):
+        if user_id in self.data:
+            raise ValueError("User already exists")
+        self.data[user_id] = name
+
+    def get_user(self, user_id):
+        return self.data.get(user_id, None)
+
+    def delete_user(self, user_id):
+        if user_id in self.data:
+            del self.data[user_id]
+        else:
+            raise ValueError("User does not exist")
+        
