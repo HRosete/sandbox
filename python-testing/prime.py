64a44932d0ff8ea897bfb9513beb238341c8fea7
Hannah Rosete <hannahmae.rosete@gmail.com>
2026-08-06 01:55:20 +1000

    add: prime.py
    Signed-off-by: Hannah Rosete <hannahmae.rosete@gmail.com>

---
+++ python-testing/prime.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    """
+    Check if a number is a prime
+    
+    Agrs:
+        n (int): The number to check
+        
+    Returns:
+        bool: True if n is prime, False otherwise
+    """
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
